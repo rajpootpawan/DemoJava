2a43bfab6f4ff192ed3fafb999781fdb6923bdcf
Pawan Singh <82039660+rajpootpawan@users.noreply.github.com>
2021-04-25 10:27:38 +0530

    Update HelloWorld.java
    Pawan added the hello mintra in remote

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,9 @@ class HelloWorld
 	public static void main(String [] args)
 	{
 		System.out.println("Hello World");
-		System.out.println("Hello Java");
+		System.out.println("Hello Java");	
+		System.out.println("Hello Mintra");
+
 
 	}
 }
